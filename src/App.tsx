140e9be05a00cb1c70350037477f99621f1af5eb
Ben Leadbetter <leadbetter.ben@outlook.com>
2025-03-26 14:40:38 +0000

    Convert to typescript

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Board from "./ui/Board";
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill("")]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  const moves = history.map((_, move) => {
+    let description = move == 0 ? "Go to game start" : "Go to move #" + move;
+
+    return (
+      <li key={move}>
+        <button onClick={() => setCurrentMove(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
--- src/ui/Board.tsx
+++ src/ui/Board.tsx
@@ -1,41 +1,8 @@
-import { useState } from "react";
+import React from "react";
+import Square from "./Square";
 
-export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  const moves = history.map((_, move) => {
-    description = move == 0 ? "Go to game start" : "Go to move #" + move;
-
-    return (
-      <li key={move}>
-        <button onClick={() => setCurrentMove(move)}>{description}</button>
-      </li>
-    );
-  });
-
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
-}
-
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
+export default function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(i: number) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -49,7 +16,7 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   const winner = calculateWinner(squares);
-  let status;
+  let status: string;
   if (winner) {
     status = "Winner: " + winner;
   } else {
@@ -75,14 +42,6 @@ function Board({ xIsNext, squares, onPlay }) {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
-  );
-}
-
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
   );
 }
 
